a604e656b39e00e594c8fb14fbed68c73b6c927c
lais <laiscristina0009@gmail.com>
2025-03-13 13:56:47 -0300

    Modificado: sistema-1.py

--- sistema-1.py
+++ sistema-1.py
@@ -24,42 +24,47 @@ Escolha a operação que deseja fazer das opções abaixo:
     [1] Sacar
     [2] Depositar
     [3] Visualizar extrato 
-    [4] Sair
-''')
+    [4] Sair 
+> ''')
 
 saldo = 0
 limite_valor_saque = 500
 extrato = ''
 num_saques = 0
-limite_num_saques = 3
+LIMITE_NUM_SAQUES = 3
 
 while True:
     escolha = input(menu)
     
     if escolha == '1': # Saque
 
-        valor_saque = int(input('Valor do saque: '))
+        valor_saque = float(input('Valor do saque: '))
 
-        excedeu_num_saque = num_saques > limite_num_saques
+        excedeu_saldo = valor_saque > saldo
+        excedeu_num_saque = num_saques > LIMITE_NUM_SAQUES
         excedeu_valor_saque = valor_saque > limite_valor_saque
 
-        if saldo > 0:
-            if valor_saque > 0:
-                if not excedeu_valor_saque:
-                    if not excedeu_num_saque:
-                        saldo -= valor_saque
-                        extrato += f'Saque: R${valor_saque:.2f}\n'
-                    else:
-                        print('&& Limite de número de saques diários (3) excedido! &&')
-                else: 
-                    print('&& Limite de valor de saque (R$500) excedido! &&')
+        if not excedeu_saldo:
+            if saldo > 0:
+                if valor_saque > 0:
+                    if not excedeu_valor_saque:
+                        if not excedeu_num_saque:
+                            saldo -= valor_saque
+                            num_saques += 1
+                            extrato += f'Saque: R${valor_saque:.2f} realizado com sucesso!\n'
+                        else:
+                            print('&& Limite de número de saques diários (3) excedido! &&')
+                    else: 
+                        print('&& Limite de valor de saque (R$500) excedido! &&')
+                else:
+                    print('&& Não é possível realizar saques com valores negativos &&')
             else:
-                print('&& Não é possível realizar saques com valores negativos &&')
+                print('&& Saldo insuficiente! Realize um depósito primeiro. &&')
         else:
-            print('&& Saldo insuficiente! Realize um depósito primeiro. &&')
+            print('&& Saque excedeu o valor do saldo! &&')
 
     elif escolha == '2': # Depósito
-        valor_deposito = int(input('Valor do depósito: '))
+        valor_deposito = float(input('Valor do depósito: '))
 
         if valor_deposito > 0:
             saldo += valor_deposito
@@ -69,7 +74,7 @@ while True:
 
     elif escolha == '3': # Extrato
         print('----------EXTRATO----------')
-        print(extrato)
+        print('Nenhuma operação realizada!' if not extrato else extrato)
         print(f'Saldo final: R${saldo:.2f}')
         print('---------------------------')
 
@@ -77,3 +82,5 @@ while True:
         print('Obridado por utilizar o nosso sistema!')
         break
 
+    else:
+        print('&& Opção inválida! Escolha uma das opções fornecidas. &&')
